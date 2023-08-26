6f394b7a823c1f4dfe49f354e3029429f0ee0fdb
Gurwinder Singh <64320086+kalersaab@users.noreply.github.com>
2023-08-26 10:46:46 +0530

    Update connection.

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ const DB_CONNECT = process.env.DB_CONNECT;
 const mongodb = (req, res) => {
   mongoose
     .connect(
-       //DB_CONNECT,
-    "mongodb://127.0.0.1:27017/user",
+       DB_CONNECT,
+    //"mongodb://127.0.0.1:27017/user",
       {
         useNewurlParser: true,
       }
