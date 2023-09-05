f3a4ce0be0c69dca12716dbff5941d0523d99a3f
kalersaab <gurwindersingh957@gmai.com>
2023-09-05 18:17:18 +0530

    bookmark

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.patch("/forget-password", forgetpassword)
 router.patch("/reset-password", resetPassword)
 
 router.post("/bookmark/", authenticateToken, CreateBookmark);
-router.get("/bookmark/:id", authenticateToken, authorization, GetBookmark);
+router.get("/bookmark", authenticateToken, GetBookmark);
 router.delete("/bookmark/:id", Bookmark.Delete);
 
 module.exports = router;
