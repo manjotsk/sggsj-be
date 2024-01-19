106ee78a075bb21dd72798ce66e870306f72f9e7
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-01-20 04:51:41 +0530

    fix

--- lambda.js
+++ lambda.js
@@ -1,8 +1,8 @@
-import awsServerlessExpress from "aws-serverless-express"
-import app from "./app.js" // Import your Express app
+import awsServerlessExpress from "aws-serverless-express";
+import app from "./app"; // Import your Express app
 const server = awsServerlessExpress.createServer(app);
 
-exports.handler = (event, context) => {
+export const handler = async (event, context) => {
   console.log("Received event:", JSON.stringify(event, null, 2));
-  return awsServerlessExpress.proxy(server, event, context, "PROMISE").promise;
+  return await awsServerlessExpress.proxy(server, event, context, "PROMISE").promise;
 };
